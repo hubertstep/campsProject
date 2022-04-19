a02ccfecfe2d6062e33ce7ffed58fefb16f13bfb
BercikHubercik <hubertstep@gmail.com>
2022-04-19 22:06:40 +0200

    added CORS headers middleware

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'camps',
     'camps_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
 
 ROOT_URLCONF = 'djangoProject.urls'
@@ -134,3 +138,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'https://localhost:3000',
+    'http://localhost:3000',
+    'localhost:3000',
+]
